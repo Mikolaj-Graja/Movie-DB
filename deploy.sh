7a89ea809582d8a47d9711d8f6cb4b2beeb9f40e
Mikołaj Graja <mikolajgraja@gmail.com>
2020-06-30 18:01:45 +0200

    edit url

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mikolaj-graja.github.io/Movie-DB.git master:gh-pages
+git push -f git@github.com:mikolaj-graja/Movie-DB.git master:gh-pages
 
 cd -
